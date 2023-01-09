b1bcf9ce7645f5a8ef3e76f92999a9a4b3f803d5
proger0014 <iakov56@yandex.ru>
2023-01-09 23:11:24 +0600

    fix tests

--- tests/UnitTests/Services/RefreshTokenServiceTests.cs
+++ tests/UnitTests/Services/RefreshTokenServiceTests.cs
@@ -8,29 +8,48 @@ public class RefreshTokenServiceTests
 {
     private RefreshTokenService CreateMock()
     {
-        var mock = new Mock<RefreshTokenRepository>();
+        var mock = new Mock<IRefreshTokenRepository>();
 
         var targetDate = new DateTime(2023, 1, 1);
 
-        mock.Setup(rr => rr.GetByUserId(1))
-            .Returns(new RefreshToken("refresh_token1", 1, "finger_print1", targetDate, targetDate.AddMinutes(1)));
+        var refreshToken1 = new RefreshToken("refresh_token1", 1, "finger_print1", targetDate, targetDate.AddMinutes(1));
+        var refreshToken2 = new RefreshToken("refresh_token2", 2, "finger_print2", targetDate.AddDays(2), targetDate.AddDays(3));
 
-        mock.Setup(rr => rr.GetByRefreshToken("refresh_token2"))
-            .Returns(new RefreshToken("refresh_token2", 2, "finger_print2", targetDate.AddDays(2), targetDate.AddDays(3)));
+        mock.Setup(rr => rr.GetByUserId(1))
+            .Returns(refreshToken1);
+
+        mock.Setup(rr => rr.GetById("refresh_token1"))
+            .Returns(refreshToken1);
+
+        mock.Setup(rr => rr.GetById("refresh_token2"))
+            .Returns(refreshToken2);
+
+        mock.Setup(rr => rr.GetByUserId(2))
+            .Returns(refreshToken2);
 
         return new RefreshTokenService(mock.Object);
     }
 
     [Fact]
-    public void GetRefreshToken_ExistsRefreshTokenId_ReturnRefreshToken()
+    public void GetRefreshToken_ByUserId_ReturnRefreshToken()
     {
         var refreshTokenService = CreateMock();
 
         var targetDate = new DateTime(2023, 1, 1);
 
         var expected = new RefreshToken("refresh_token1", 1, "finger_print1", targetDate, targetDate.AddMinutes(1));
-        var actual = refreshTokenService.GetRefreshToken("refresh_token2");
+        var actual = refreshTokenService.GetRefreshTokenByUserId(1);
 
         Assert.Equal(expected, actual);
+    }
+
+    public void GetRefreshToken_ById_ReturnRefreshToken()
+    {
+        var refresTokenService = CreateMock();
+
+        var targetDate = new DateTime(2023, 1, 1);
+        
+        var expected = new RefresToken("refresh_token2", 1, "finger_print2", targetDate.AddDays(2), targetDate.AddDays(3));
+        var actual = refresTokenService.GetById();
     }
 }
